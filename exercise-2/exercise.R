870baae715f5a3f4a4f19117912adf5dba237656
adewolff <30436922+adewolff@users.noreply.github.com>
2018-04-23 16:12:15 -0700

    finished exercise-2

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,15 +1,16 @@
 # Exercise 2: working with data APIs
 
 # load relevant libraries
+library(dplyr)
 library(httr)
 library(jsonlite)
 
 # Use `source()` to load your API key variable from the `apikey.R` file you made.
 # Make sure you've set your working directory!
-
+source("apikey.r")
 
 # Create a variable `movie.name` that is the name of a movie of your choice.
-
+movie_name <- "The Grand Budapest Hotel"
 
 # Construct an HTTP request to search for reviews for the given movie.
 # The base URI is `https://api.nytimes.com/svc/movies/v2/`
@@ -20,25 +21,38 @@ library(jsonlite)
 # You should use YOUR api key (as the `api-key` parameter)
 # and your `movie.name` variable as the search query!
 
+base_uri <- "https://api.nytimes.com/svc/movies/v2/"
+resource <- "reviews/search.json"
+query_params <- list(query = movie_name, "api-key" = nyt_apikey)
 
 # Send the HTTP Request to download the data
 # Extract the content and convert it from JSON
-
+response <- GET(paste0(base_uri, resource), query = query_params)
+body <- fromJSON(content(response, "text"))
 
 # What kind of data structure did this produce? A data frame? A list?
-
+typeof(body)
 
 # Manually inspect the returned data and identify the content of interest 
 # (which are the movie reviews).
 # Use functions such as `names()`, `str()`, etc.
-
-
+names(body)
+str(body)
 # Flatten the movie reviews content into a data structure called `reviews`
-
+reviews <- flatten(body$results)
 
 # From the most recent review, store the headline, short summary, and link to
 # the full article, each in their own variables
-
+recent_headline <- reviews %>% filter(publication_date == max(publication_date)) %>% 
+  pull(headline)
+recent_short_summary <- reviews %>% filter(publication_date == max(publication_date)) %>% 
+  pull(summary_short)
+recent_article_link <- reviews %>% filter(publication_date == max(publication_date)) %>% 
+  pull(link.url)
 
 # Create a list of the three pieces of information from above. 
 # Print out the list.
+query_list <- list("headline" = recent_headline, 
+                   "short_summary" = recent_short_summary, 
+                   "article link" = recent_article_link)
+query_list
